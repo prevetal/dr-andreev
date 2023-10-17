73dd6ffa179e68fb22dc724cf227a3c64732a0ee
Vitaliy Sulimenko <prevetal@gmail.com>
2023-10-17 16:44:10 +0200

    Изменения

--- js/functions.js
+++ js/functions.js
@@ -5,29 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
 	}
 
 
-	// Ленивая загрузка
-	const boxes = document.querySelectorAll('.lazyload, .animate')
-
-	function scrollTracking(entries) {
-		for (const entry of entries) {
-			if (entry.intersectionRatio >= 0.2 && !entry.target.classList.contains('loaded')) {
-				entry.target.src = entry.target.getAttribute('data-src')
-				entry.target.classList.add('loaded')
-			}
-
-			if (entry.intersectionRatio >= 0.2 && entry.target.classList.contains('animate')) {
-				entry.target.classList.add('animated')
-			}
-		}
-	}
-
-	const observer = new IntersectionObserver(scrollTracking, {
-		threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-	})
-
-	boxes.forEach(element => observer.observe(element))
-
-
 	// Установка ширины стандартного скроллбара
 	document.documentElement.style.setProperty('--scroll_width', widthScroll() + 'px')
 
--- js/scripts.js
+++ js/scripts.js
@@ -256,6 +256,39 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+
+window.addEventListener('load', function () {
+	// Ленивая загрузка
+	const boxes = document.querySelectorAll('.lazyload')
+
+	function scrollTracking(entries) {
+		for (const entry of entries) {
+			if (entry.intersectionRatio >= 0.2 && !entry.target.classList.contains('loaded')) {
+				if(entry.target.localName === 'video') {
+					let sources = entry.target.querySelectorAll('source')
+
+					sources.forEach(source => {
+						source.src = source.getAttribute('data-src')
+						entry.target.load()
+					})
+				} else {
+					entry.target.src = entry.target.getAttribute('data-src')
+				}
+
+				entry.target.classList.add('loaded')
+			}
+		}
+	}
+
+	const observer = new IntersectionObserver(scrollTracking, {
+		threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+	})
+
+	boxes.forEach(element => observer.observe(element))
+})
+
+
+
 window.addEventListener('scroll', function () {
 	// Кнопка 'Вверх'
 	let buttonUp = document.querySelector('.buttonUp')
